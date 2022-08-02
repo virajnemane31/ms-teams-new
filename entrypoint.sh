88a6e7dade09160f90a41c409d75ce8538b3b9fc
virajnemane31 <viraj.nemane@genmills.com>
2022-08-02 12:06:26 +0530

    v5

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 data='{"text":"'${2}'"}'
 echo $data > message.txt
 #curl $1 -X post -H "Content-type: application/json" --data "@message.txt"
-curl -H "Content-Type: application/json" -X POST -d '{"user":"bob","pass":"123"}' $1
+curl -H "Content-Type: application/json" -X POST --data "@message.txt" $1
